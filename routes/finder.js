fc7cc6f39380a991bf7060f6ae05c11106033b30
Martin Hernan Gomez <mhgomez@fi.uba.ar>
2024-05-24 22:53:26 -0300

    Adds swagger-express annotations

--- routes/finder.js
+++ routes/finder.js
@@ -4,9 +4,34 @@
 */
 
 const {Router} = require('express');
+
+/**
+ * @swagger
+ * tags:
+ *   - name: Finder
+ *     description: API para hacer uso de la funcionalidad match finder.
+ */ 
 const router = Router();
 const getCandidates = require('../controllers/finder/getCandidates');
 
+/**
+ * @swagger
+ * /api/finder/candidates/:
+ *   get:
+ *     summary: Lista de candidatos
+ *     description: Devuelve la lista de candidatos con su perfil completo
+ *     parameters:
+ *       - in: query
+ *         name: profileId
+ *         description: The user profile identifier
+ *         schema:
+ *           type: string
+ *         required: true
+ *     tags: [Finder]
+ *     responses:
+ *       200:
+ *         description: Una lista de candidatos
+ */
 router.get('/candidates', getCandidates.validation, getCandidates.handler);
 
 
