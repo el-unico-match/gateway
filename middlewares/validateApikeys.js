c3be1f583b1a1a9535d38296c7a35ee81262ad46
Martin Hernan Gomez <mhgomez@fi.uba.ar>
2024-06-27 01:38:17 -0300

    Removes unused methods

--- middlewares/validateApikeys.js
+++ middlewares/validateApikeys.js
@@ -1,46 +1,7 @@
-const {response} = require('express');
 const jwt = require('jsonwebtoken');
-const {HTTP_CLIENT_ERROR_4XX, HTTP_SERVER_ERROR_5XX} = require('../helpers/httpCodes')
-const {getApikeys,getSelfApikey, isApiKeyCheckingEnabled} = require('../helpers/apikeys');
-const {MSG_NO_APIKEY, 
-    MSG_INVALID_APIKEY,
-    MSG_APIKEY_NO_MATCH} = require('../messages/apikey.js');
-const {
-    logInfo,
-    logDebug} = require('../helpers/log/log');
-    
-/**
- * Valida el x-apikey del request y compara con las apikeys locales validando
- * la propia.
- */
-const validateApikeys = (req, res = response, next) => {
-
-    const requestApikey = req.header('x-apikey');
-    logDebug(`On validate apikey: ${requestApikey}`);
-    if (!requestApikey) {
-        const dataToResponse = {
-            ok: false,
-            msg: MSG_NO_APIKEY
-        };
-        logDebug(`On validate apikey null`);
-        logInfo(`On validate apikey response: ${HTTP_CLIENT_ERROR_4XX.BAD_REQUEST}; ${JSON.stringify(dataToResponse)}`)
-        return res.status(HTTP_CLIENT_ERROR_4XX.BAD_REQUEST).json(dataToResponse);
-    } 
-    try {
-        doValidateApikey(requestApikey);
-    } catch (error) {
-        const dataToResponse = {
-            ok: false,
-            msg: MSG_INVALID_APIKEY
-        };
-        const statusToResponse = HTTP_SERVER_ERROR_5XX.SERVICE_NOT_AVAILABLE;
-        logDebug(`On validate request apikey error: ${error}`);
-        logInfo(`On validate request apikey response: ${statusToResponse}; ${JSON.stringify(dataToResponse)}`)
-        return res.status(statusToResponse).json(dataToResponse)
-    }; 
-
-    next();
-}
+const {getApikeys} = require('../helpers/apikeys');
+const {MSG_APIKEY_NO_MATCH} = require('../messages/apikey.js');
+const {logDebug} = require('../helpers/log/log');
 
 /**
  * 
@@ -62,13 +23,7 @@ const doValidateApikey = (apikeyToValidate) =>  {
     }
 }
 
-const validateApikeysOnResponse = () => {
-    doValidateApikey(getSelfApikey());
-}
-
 module.exports = {
     doValidateApikey,
-    validateApikeys,
-    validateApikeysOnResponse
 }
 
